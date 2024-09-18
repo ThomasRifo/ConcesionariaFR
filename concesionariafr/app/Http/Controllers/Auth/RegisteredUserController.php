36dd00d52d6a5bcb2d1850f7d8cc0f7dc7d6e076
juliaanbl <julianblancokriger@gmail.com>
2024-09-18 14:56:19 -0300

    paquete spatie

--- concesionariafr/app/Http/Controllers/Auth/RegisteredUserController.php
+++ concesionariafr/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,7 +46,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'activo' => true, 
-        ]);
+        ])->assignRole('cliente');
 
         event(new Registered($user));
 
